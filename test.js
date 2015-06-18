22da044f18c4d60a4fa2543becfa82d7e77f3f91
JP Richardson <jprichardson@gmail.com>
2015-06-18 16:05:20 -0500

    fixtures: revised

--- test.js
+++ test.js
@@ -5,20 +5,23 @@ var fixtures = require('./fixtures')
 
 /* global before, describe, it */
 
-describe('reviver-buffer', function () {
+describe('buffer-json', function () {
   before(function () {
     assert(semver.gte(process.version, '0.11.0'), 'Bad Node.js/IO.js version. You need to use at least v0.11.')
   })
 
-  fixtures.valid.forEach(function (f) {
-    it('should convert ' + f.value, function () {
-      // verify fixture is correct
-      var b = new Buffer(f.value, f.type)
-      assert.deepEqual(b.toJSON(), f.json)
+  describe('reviver()', function () {
+    fixtures.valid.forEach(function (f) {
+      it('should convert ' + f.value, function () {
+        // verify fixture is correct
+        var b = new Buffer(f.utf8)
+        assert.deepEqual(b.toJSON(), f.json)
 
-      var b2 = JSON.parse(JSON.stringify(f.json), reviver)
-      assert.strictEqual(b.toString('utf8'), b2.toString('utf8'))
-      assert.strictEqual(b.toString('hex'), b2.toString('hex'))
+        var b2 = JSON.parse(JSON.stringify(f.json), reviver)
+        assert.strictEqual(b.toString('utf8'), b2.toString('utf8'))
+        assert.strictEqual(b.toString('hex'), b2.toString('hex'))
+        assert.strictEqual(b.toString('base64'), b2.toString('base64'))
+      })
     })
   })
 })
